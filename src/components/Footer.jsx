d64b32c5f95b8631a90ff2951c4569b5fa90b97a
mshulezhko <119418285+mshulezhko@users.noreply.github.com>
2024-01-05 21:25:51 +0100

    add teg a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      Challenge by <a target='_blank' href='https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH' >Frontend Mentor.</a> Coded by Mariia Shulezhko.
+      Challenge by <a rel="noreferrer" target='_blank' href='https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH' >Frontend Mentor.</a> Coded by Mariia Shulezhko.
     </footer>
   )
 }
